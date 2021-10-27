7362a1e0f8ab70fd87ff54b53afe91fd91b0af4d
AnimalRacional <88465145+AnimalRacional@users.noreply.github.com>
2021-10-27 20:17:02 +0100

    Adicionado verificacao basica das respostas

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,54 @@
+// Verifica se uma string é um número
+function isNumeric(num){
+    if(typeof(num) != "string"){return false;}
+    for(let i = 0; i < num.length; i++){
+        if(num[i] < '0' || num[i] > '9'){ return false; }
+    }
+    return true && num.length > 0;
+}
+
+// Verifica se é um email válido
+function isValidEmail(email){
+    let dotSplit = email.split('.');
+    return !(dotSplit.length-1 != 3 || email.split('@').length-1 != 1 || !isNumeric(dotSplit[0]) && email.endsWith("@eped.pt"))
+}
+
+// valida as respostas todas
+function validateAnswers(){
+    let frm = document.forms["formulario"]
+    for(let i = 0; i < frm.length; i++){
+        let cur = frm[i];
+        if(cur.type == "email"){
+            if(!isValidEmail(cur.value)){
+                alert("Email inválido! Por favor use o seu e-mail institucional.")
+                return false;
+            }
+        }
+        else if(frm[i].type == "radio"){
+            curRadio = [cur]
+            for(i = i+1; i < frm.length; i++){
+                if(frm[i].type == "radio" && frm[i].name == cur.name){
+                    curRadio.push(frm[i]);
+                }
+                else{
+                    break;
+                }
+            }
+            if(curRadio[curRadio.length - 1].value != "Other") { i--; }
+            result = "";
+            for(let j = 0; j < curRadio.length; j++){
+                if(curRadio[j].checked){result = curRadio[j].value; break;}
+            }
+            if(result == "Other"){
+                result = frm[i].value;
+            }
+            if(result == ""){ alert("Escolha Inválida!"); return false;}
+        }
+        
+    }
+    return false;
+}
+
 /*
 parâmetros:
 label -> A pergunta
@@ -8,11 +59,11 @@ other -> se tem ou não um botão "Outro:"
 function radioButton(label, name, options, other=true){
     document.write("<label class=\"question-label\" for=\"" + name + "\">" + label + "</label><br>");
     options.forEach(cur => {
-        document.write("<input class=\"question-input\" type=\"radio\" id=\"" + name.toLowerCase() + cur.toLowerCase() + "\" name=\"" + name + "\" value=\"" + cur + "\">");
+        document.write("<input required class=\"question-input\" type=\"radio\" id=\"" + name.toLowerCase() + cur.toLowerCase() + "\" name=\"" + name + "\" value=\"" + cur + "\">");
         document.write("<label class=\"radio-label\" for=\"" + name.toLowerCase() + cur.toLowerCase() + "\"> " + cur + "</label><br>")
     });
     if(other){
-        document.write("<input class=\"question-input\" type=\"radio\" id=\"other" + name.toLowerCase() + "\" name=\"" + name + "\" value=\"Other\">");
+        document.write("<input required class=\"question-input\" type=\"radio\" id=\"other" + name.toLowerCase() + "\" name=\"" + name + "\" value=\"Other\">");
         document.write("<label class=\"radio-label\" for=\"other\"> Outro</label> ")
         document.write("<input class=\"question-other-text\" type=\"text\" id=\"other" + name.toLowerCase() + "text\"><br>")
     }
@@ -28,6 +79,6 @@ function textBox(label, name){
         "<label class=\"question-label\" for=\"" + name.toLowerCase() + "\">" + label + "</label><br>"
     )
     document.write(
-        "<input class=\"question-input\" id=\"" + name.toLowerCase() + "\" name=\"" + name + "\" type=\"text\"><br>"
+        "<input required class=\"question-input\" id=\"" + name.toLowerCase() + "\" name=\"" + name + "\" type=\"text\"><br>"
     )
 }
