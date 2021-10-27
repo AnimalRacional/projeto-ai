e2a313df1d682893cfb681b8f63b00c1c3397ec0
AnimalRacional <88465145+AnimalRacional@users.noreply.github.com>
2021-10-27 18:35:09 +0100

    Adicionada textBox() para fazer perguntas de texto mais facilmente

--- scripts/main.js
+++ scripts/main.js
@@ -8,12 +8,26 @@ other -> se tem ou não um botão "Outro:"
 function radioButton(label, name, options, other=true){
     document.write("<label class=\"question-label\" for=\"" + name + "\">" + label + "</label><br>");
     options.forEach(cur => {
-        document.write("<input class=\"question-input\" type=\"radio\" id=\"" + cur.toLowerCase() + "\" name=\"" + name + "\" value=\"" + cur + "\">");
-        document.write("<label class=\"radio-label\" for=\"" + cur.toLowerCase() + "\"> " + cur + "</label><br>")
+        document.write("<input class=\"question-input\" type=\"radio\" id=\"" + name.toLowerCase() + cur.toLowerCase() + "\" name=\"" + name + "\" value=\"" + cur + "\">");
+        document.write("<label class=\"radio-label\" for=\"" + name.toLowerCase() + cur.toLowerCase() + "\"> " + cur + "</label><br>")
     });
     if(other){
         document.write("<input class=\"question-input\" type=\"radio\" id=\"other" + name.toLowerCase() + "\" name=\"" + name + "\" value=\"Other\">");
         document.write("<label class=\"radio-label\" for=\"other\"> Outro</label> ")
         document.write("<input class=\"question-other-text\" type=\"text\" id=\"other" + name.toLowerCase() + "text\"><br>")
     }
+}
+
+/*
+parâmetros:
+label -> a pergunta
+name -> o id das text boxes
+*/
+function textBox(label, name){
+    document.write(
+        "<label class=\"question-label\" for=\"" + name.toLowerCase() + "\">" + label + "</label><br>"
+    )
+    document.write(
+        "<input class=\"question-input\" id=\"" + name.toLowerCase() + "\" name=\"" + name + "\" type=\"text\"><br>"
+    )
 }
