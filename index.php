a749ce38f040ffe80509a1d6803d026d899cb4e4
AnimalRacional <88465145+AnimalRacional@users.noreply.github.com>
2021-12-01 10:40:13 +0000

    Adicionadas as perguntas do questionario

--- index.php
+++ index.php
@@ -18,8 +18,17 @@
             <h6>O seu e-mail não vai ser associado às suas respostas, e vai ser apenas utilizado para identificar quem respondeu ao questionário</h6>
 
             <script>
-                textBox("Pergunta genérica", "pergunta_generica")
-                radioButton("Bom dia", "radio_button_test", ["test", "trjei", "wow"])
+                numeric("Idade", "idade");
+                textBox("Nacionalidade", "nacionalidade");
+                radioButton("Género", "genero", ["Masculino", "Feminino"], true);
+                radioButton("Identidade de Género", "identidadegenero", ["Cisgénero", "Transsexual"])
+                radioButton("Orientação Sexual", "sexualidade", ["Heterossexual", "Homossexual", "Bisexual", "Asexual", "Pansexual"], true)
+                textBox("Religião", "religiao");
+                radioButton("É praticante da sua religião?", "praticante", ["Sim", "Não", "Sou ateu/agnóstico"], false);
+                textBox("Hobbies?", "hobbies");
+                radioButton("Qual o seu plano para o futuro?", "planofuturo", ["Faculdade", "Trabalho"], true);
+                textBox("Resumidamente, qual a sua opinião da EPED?", "opiniaoescola");
+                textBox("Disciplina Favorita", "disciplina")
             </script>
 
             <input class="submit-button" value="Submeter" id="submeter" type="submit">
